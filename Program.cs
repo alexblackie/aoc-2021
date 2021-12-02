02e5f1869a9213c14a8bee29072a9826c3c4b08a
Alex Blackie <alex@blackie.me>
2021-12-02 10:51:16 -0800

    Add Day 2 input and call from Main

--- Program.cs
+++ Program.cs
@@ -8,11 +8,17 @@ class Program
     static void Main(string[] args)
     {
 		var ingestor = new Ingestor();
+
+		// Day 1
 		int[] input = ingestor.ReadNumbers("Inputs/Day1-1.txt");
+		var depth = new DepthComparator();
+		Console.WriteLine($"Day 1: {depth.CountIncreases(input)}");
+		Console.WriteLine($"Day 1.1: {depth.CountIncreasesThrice(input)}");
 
-		var service = new DepthComparator();
-
-		Console.WriteLine($"Day 1: {service.CountIncreases(input)}");
-		Console.WriteLine($"Day 1.1: {service.CountIncreasesThrice(input)}");
+		// Day 2
+		var sub = new SubmarineVehicle();
+		var directions = ingestor.ReadDirectionChanges("Inputs/Day2.txt");
+		sub.Move(directions);
+		Console.WriteLine($"Day 2: X:{sub.X} Z:{sub.Z} Product:{sub.X * sub.Z}");
 	}
 }
